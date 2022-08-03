2099c5aba4e96aa199a63d78d0ce36c99778552b
tanb01 <tanb0101@gmail.com>
2022-08-03 17:49:49 -0400

    Fixed issue dotenv issue

---
+++ services/fireinit.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/database'
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
+
+!firebase.apps.length ? firebase.initializeApp(config) : ''
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+export const auth = firebase.auth()
+export const DB = firebase.database()
+export const StoreDB = firebase.firestore()
+export default firebase
